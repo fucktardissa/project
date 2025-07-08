4cc53fea4b1aaab1d2e9eb9b33194a73a8aeef2c
fucktardissa <lonlyalt1@gmail.com>
2025-07-08 09:30:20 -0400

    Update t.lua

--- t.lua
+++ t.lua
@@ -12,7 +12,7 @@ local TARGET_EGG_NAME = "festival-rift-3"
 local SUCCESS_WEBHOOK_URL = "https://ptb.discord.com/api/webhooks/1391330776389259354/8W3Cphb1Lz_EPYiRKeqqt1FtqyhIvXPmgfRmCtjUQtX6eRO7-FuvKAVvNirx4AizKfNN"
 local FAILURE_WEBHOOK_URL = "https://ptb.discord.com/api/webhooks/1391330776389259354/8W3Cphb1Lz_EPYiRKeqqt1FtqyhIvXPmgfRmCtjUQtX6eRO7-FuvKAVvNirx4AizKfNN"
 local EGG_THUMBNAIL_URL = "https://www.bgsi.gg/eggs/july4th-egg.png"
-local VERTICAL_SPEED = 150
+local VERTICAL_SPEED = 500
 local HORIZONTAL_SPEED = 35
 
 -- =============================================
